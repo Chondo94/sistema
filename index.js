8aa8c0894829bbc6cd8b6a8a3423bc02edac14d1
Chondo94 <estuardobojorquezcortez@gmail.com>
2020-10-06 17:44:29 -0600

    Agregue Babel.js que me ayuda a transpilar o traducir el codigo de ES6 a ES5, aca actualice a ES6

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 // Mi archivo JS para agregar la configuracion de mi servidor express
 
+/* 
 // aca requiero mi modulo express y lo guardo en la constante
 const express=require('express');
+*/
+/* 
 // voy a requerir la dependencia de morgan
 const morgan=require('morgan');
+*/
+/* 
 const cors=require('cors');
+*/
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
+/* 
 const app=express();
 // indico que utilizo morgan en modo dev(desarrollo)
 app.use(morgan('dev'));
 // agregamos cors
 app.use(cors());
+ */
 
 // es necesario agregar middleware express json, si vamos a recibir datos por medio de un formulario mediante Json
 // y esto es mediantes peticiones por medio de POST.
 // express json es una funcion agregada en el frameworks express
-app.use(express.js());
-app.use(express.urlencoded({extended:true}));
+const app=express();
+app.use(morgan('dev'));
+app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 /*  con mi objeto, creo listen para que me escuche en el puerto 300 o en el puerto que es 
     asigando por mi servidor y luego paso a que me ejecute una funcion donde quier que me muestre un mensaje por consola. 
 */
+
 /* 
 app.set('port',process.env.PORT || 3000);
 app.listen(app.get('por'),()=>{
@@ -52,18 +67,10 @@ mongoose.connect(dbUrl, {useCreateIndex:true, useNewUrlParser: true})
  */
 
 /* 
-const app=express();
-app.use(morgan('dev'));
-app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')))
 
 app.use('/api',router);
-app.set('port',process.env.PORT || 3000);
 
-app.listen(app.get('port'),()=>{
-    console.log('server on port ' + app.get('port'));
 });
  */
