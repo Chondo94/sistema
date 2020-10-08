5cd2b2fa82440d5983d7076e02c53caf1b14ea91
Chondo94 <estuardobojorquezcortez@gmail.com>
2020-10-07 19:04:22 -0600

    Agregue mi modelo usuario

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@
 import routerx from 'express-promise-router';
 // improta el objeto categoriaRoute
 import categoriaRouter from './categoria';
+// importo el objeto de articuloRoute, yo le doy el nombre de articuloRouter haciendo referencias a las rutas que tengo en articulo
+import articuloRouter from './articulo';
 
 // declaro mi contaste router
 const router=routerx();
@@ -11,4 +13,5 @@ const router=routerx();
 // aca le indico que cuando utilice el /categoria las rutas se van a gestionar con el
 // categoriaRouter que viene siendo el categoria.js
 router.use('/categoria',categoriaRouter);
+router.use('/articulo',articuloRouter);
 export default router;
