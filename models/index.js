d9ed73453bd9950776f1c69b292aa62b8dd1fdaf
Chondo94 <estuardobojorquezcortez@gmail.com>
2020-10-07 22:58:44 -0600

    Agregue el modelo persona, que me va servir tanto para clientes como para proveedores.

--- models/index.js
+++ models/index.js
@@ -3,8 +3,11 @@
 import Categoria from './categoria';
 import Articulo from './articulo'
 import Usuario from  './usuario'
+import Persona from './persona';
+
 export default {
     Categoria,
     Articulo,
-    Usuario
+    Usuario,
+    Persona
 }
